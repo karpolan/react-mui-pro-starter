6f4f47cb1f1a830f2d629776487a052459b76981
Anton Karpenko <karpolan@karpolan.com>
2021-05-13 15:51:10 +0300

    fix: Dialogs -> dialogs

---
+++ src/components/renamed-dialogs/components/AppDialogTitle.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { makeStyles, DialogTitle } from '@material-ui/core';
+import { AppIconButton } from '../..';
+import { dialogStyles } from '../../../utils/styles';
+
+const useStyles = makeStyles((theme) => ({
+  ...dialogStyles(theme),
+}));
+
+/**
+ * Renders Material UI Dialog Title with optional (x) button to close the dialog
+ * @param {func} [props.onClose] - when set the (x) button aded to Dialog Title and event called on button click
+ */
+const AppDialogTitle = ({ children, onClose, ...props }) => {
+  const classes = useStyles();
+  return (
+    <DialogTitle {...props}>
+      {children}
+      {Boolean(onClose) ? (
+        <AppIconButton className={classes.xButton} icon="close" aria-label="Close" title="Close" onClick={onClose} />
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+AppDialogTitle.propTypes = {
+  onClose: PropTypes.func,
+};
+
+export default AppDialogTitle;
